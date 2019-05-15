f7f39b18af449d54d796632bd280efcad56d151c
araarakelyan <ara.araqelyan90@gmail.com>
2019-05-15 18:07:49 +0400

    added missed file

---
+++ sw.js
@@ -0,0 +1,25 @@
+// Give the service worker access to Firebase Messaging.
+// Note that you can only use Firebase Messaging here, other Firebase libraries
+// are not available in the service worker.
+importScripts('https://www.gstatic.com/firebasejs/6.0.0/firebase-app.js','https://www.gstatic.com/firebasejs/6.0.0/firebase-messaging.js');
+
+
+// Initialize Firebase
+firebase.initializeApp({messagingSenderId: "330328842011"});
+const messaging = firebase.messaging();
+
+
+messaging.setBackgroundMessageHandler(function(payload) {
+    console.log('[firebase-messaging-sw.js] Received background message ', payload);
+    // Customize notification here
+    var notificationTitle = 'Background Message Title';
+    var notificationOptions = {
+        body: 'Background Message body.',
+        title: 'Background Message title.',
+        icon: '/firebase-logo.png'
+    };
+
+    return self.registration.showNotification(notificationTitle,
+        notificationOptions);
+});
+
